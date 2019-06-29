4cff41cf5d681383058d270cd4554deffb3247c8
shihjie shyu <shih_jie@outlook.com>
2019-06-29 10:52:27 +0800

    胖子 完成版

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -15,7 +15,8 @@ namespace DependencyInjectionWorkshop.Models
         {
 
             var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
-
+            
+            #region 登入前檢查是否被鎖
             var isLockedResponse = httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", account).Result;
 
             isLockedResponse.EnsureSuccessStatusCode();
@@ -23,18 +24,22 @@ namespace DependencyInjectionWorkshop.Models
             {
                 throw new FailedTooManyTimesException();
             }
+            #endregion
 
             string verifyPasswordFromDb;
             string verifyPasswordFromHash;
             string verifyOtp;
 
+            #region 從DB內取hash過後的password
             using (var connection = new SqlConnection("my connection string"))
             {
                 verifyPasswordFromDb = connection.Query<string>("spGetUserPassword", new { Id = account },
                     commandType: CommandType.StoredProcedure).SingleOrDefault();
             }
+            #endregion
 
 
+            #region 把使用者輸入的password做hash
             var crypt = new System.Security.Cryptography.SHA256Managed();
             var hash = new StringBuilder();
             var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
@@ -43,8 +48,10 @@ namespace DependencyInjectionWorkshop.Models
                 hash.Append(theByte.ToString("x2"));
             }
             verifyPasswordFromHash = hash.ToString();
+            #endregion
 
 
+            #region 從service 拿account當下的otp
             var response = httpClient.PostAsJsonAsync("api/otps", account).Result;
             if (response.IsSuccessStatusCode)
             {
@@ -54,34 +61,42 @@ namespace DependencyInjectionWorkshop.Models
             {
                 throw new Exception($"web api error, accountId:{account}");
             }
+            #endregion
 
+            #region 驗證成功
             if (otp.Equals(verifyOtp) && verifyPasswordFromDb.Equals(verifyPasswordFromHash))
             {
+                #region 登入成功後 重製失敗次數
                 //成功
                 var resetResponse = httpClient.PostAsJsonAsync("api/failedCounter/Reset", account).Result;
                 resetResponse.EnsureSuccessStatusCode();
+                #endregion
 
                 return true;
             }
+            #endregion
 
+            #region 登入失敗發送slack通知
             var slackClient = new SlackClient("my api token");
             slackClient.PostMessage(slackResponse => { }, "my channel", "my message", "my bot name");
-            
+            #endregion
+
+            #region 登入失敗新增失敗紀錄
             //失敗
             var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", account).Result;
             addFailedCountResponse.EnsureSuccessStatusCode();
+            #endregion
 
-
+            #region 取得登入失敗次數並且寫log
             var failedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", account).Result;
             failedCountResponse.EnsureSuccessStatusCode();
 
             var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
             var logger = NLog.LogManager.GetCurrentClassLogger();
             logger.Info($"accountId:{account} failed times:{failedCount}");
-
+            #endregion
 
             return false;
-            
         }
     }
 
