b019a0235241edf5e2a98031fe969226eca6e61a
shihjie shyu <shih_jie@outlook.com>
2019-06-29 10:14:26 +0800

    AuthenticationService.Verify 胖子

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,51 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string account, string password, string otp)
+        {
+            string verifyPasswordFromDb;
+            string verifyPasswordFromHash;
+            string verifyOtp;
+
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                verifyPasswordFromDb = connection.Query<string>("spGetUserPassword", new { Id = account },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            verifyPasswordFromHash = hash.ToString();
+
+
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", account).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                verifyOtp = response.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{account}");
+            }
+
+
+            return otp.Equals(verifyOtp) && verifyPasswordFromDb.Equals(verifyPasswordFromHash);
+        }
     }
 }
